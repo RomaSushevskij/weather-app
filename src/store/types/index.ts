55605ed7aab3b9024a97b419b1a57ce764ff5baa
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-08 14:54:26 +0300

    Creating ActionType

--- src/store/types/index.ts
+++ src/store/types/index.ts
@@ -2,3 +2,7 @@ import { rootReducer, store } from 'store/store';
 
 export type AppStateType = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
+export type ActionType<T, P> = {
+  type: T;
+  payload?: P;
+};
