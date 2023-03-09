39d46f07cc4a315017c9af05f4f2317ea58a66f5
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-09 16:11:16 +0300

    some testing in App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import style from './App.module.scss';
+
+import { WeatherBlock } from 'components/WetherBlock';
 import { getCurrentWeather } from 'store/sagas/weather/weatherSagas';
 import { ReturnComponent } from 'types';
 
@@ -12,7 +15,46 @@ const App = (): ReturnComponent => {
     dispatch(getCurrentWeather());
   }, []);
 
-  return <div>App</div>;
+  return (
+    <div className={style.app}>
+      <div style={{ height: 300 }} />
+      <div style={{ display: 'flex' }}>
+        <WeatherBlock
+          isCurrentDate
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/clear-day.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/clear-day.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/partly-cloudy-day.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/cloudy.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/rain.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/cloudy.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/snow.svg"
+          date="today"
+        />
+        <WeatherBlock
+          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/wind.svg"
+          date="today"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
