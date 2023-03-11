49483ffe2545e53e195435e7689566e81e855d59
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:08:04 +0300

    Create LocationFromState type. Changing imports

--- src/store/reducers/weather/types.ts
+++ src/store/reducers/weather/types.ts
@@ -1,9 +1,16 @@
-import { WeatherCondition } from 'api';
-import { weatherIcons } from 'api/weatherAPI/enums';
+import { WeatherCondition } from 'api/weatherAPI';
+import { weatherIcons } from 'api/weatherAPI';
 import { Nullable } from 'types';
 
 export type CurrentWeather = {
   icon: Nullable<weatherIcons>;
   temp: Nullable<number>;
   hourlyWeather: WeatherCondition[];
+};
+
+export type LocationFromState = {
+  cityName: Nullable<string>;
+  latitude: Nullable<number>;
+  longitude: Nullable<number>;
+  country: Nullable<string>;
 };
