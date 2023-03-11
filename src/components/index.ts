cac4c5e2e651e9d813cdafce55c76ff18b5e005d
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:33:27 +0300

    Reexport of components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Chip } from './Chip';
+export { DateDisplay } from './DateDisplay';
+export { Location } from './Location';
+export { WeatherFrame } from './WeatherFrame';
+export { WeatherList } from './WeatherList';
+export { WeatherBlock } from './WetherBlock';
+export { WeatherIcon } from './WetherBlock/WeatherIcon';
