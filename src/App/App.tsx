fe91fdb0a95f10db0da55efe9a03d6ef2f7a210f
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:35:23 +0300

    App for testing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,58 +1,19 @@
-import { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
 import style from './App.module.scss';
 
-import { WeatherBlock } from 'components/WetherBlock';
-import { getCurrentWeather } from 'store/sagas/weather/weatherSagas';
+import { WeatherFrame } from 'components';
 import { ReturnComponent } from 'types';
 
 const App = (): ReturnComponent => {
-  const dispatch = useDispatch();
+  // const dispatch = useAppDispatch();
+  // const weatherData = useAppSelector(state => state.weatherSelectors.currentWeather.hourlyWeather);
 
-  useEffect(() => {
-    dispatch(getCurrentWeather());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(getCurrentWeather());
+  // }, []);
 
   return (
     <div className={style.app}>
-      <div style={{ height: 300 }} />
-      <div style={{ display: 'flex' }}>
-        <WeatherBlock
-          isCurrentDate
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/clear-day.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/clear-day.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/partly-cloudy-day.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/cloudy.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/rain.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/cloudy.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/snow.svg"
-          date="today"
-        />
-        <WeatherBlock
-          iconSrc="https://raw.githubusercontent.com/RomaSushevskij/weatherIcons/master/wind.svg"
-          date="today"
-        />
-      </div>
+      <WeatherFrame />
     </div>
   );
 };
