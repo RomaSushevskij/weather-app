2f58d255235fa3fb1aea9737ff210a10b8086226
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:11:05 +0300

    WeatherBlock component: - adding of 'temp', 'icon' props

--- src/components/WetherBlock/WeatherBlock.tsx
+++ src/components/WetherBlock/WeatherBlock.tsx
@@ -2,28 +2,35 @@ import { memo } from 'react';
 
 import style from './WeatherBlock.module.scss';
 
+import { weatherIcons } from 'api/weatherAPI';
 import { Chip } from 'components/Chip';
 import { WeatherIcon } from 'components/WetherBlock/WeatherIcon/WeatherIcon';
+import { getWeatherIconLink } from 'utils';
 
 type WeatherBlockProps = {
   date: string;
+  temp: number;
+  icon: weatherIcons;
   isCurrentDate?: boolean;
-  iconSrc: string;
 };
 
 export const WeatherBlock = memo(
-  ({ isCurrentDate, iconSrc, date }: WeatherBlockProps) => {
+  ({ isCurrentDate, icon, date, temp }: WeatherBlockProps) => {
     const weatherBlockStyle = isCurrentDate
       ? `${style.weatherBlock} ${style.current}`
       : style.weatherBlock;
 
+    const iconSrc = getWeatherIconLink(icon);
+
     return (
       <div className={weatherBlockStyle}>
-        {isCurrentDate && <WeatherIcon src={iconSrc} isCurrent={isCurrentDate} />}
+        {isCurrentDate && (
+          <WeatherIcon src={iconSrc} isCurrent={isCurrentDate} alt={icon} />
+        )}
         <div className={style.weatherInfo}>
           <Chip label={date} size={isCurrentDate ? 'medium' : 'small'} />
-          {!isCurrentDate && <WeatherIcon src={iconSrc} />}
-          <p className={style.temp}>12</p>
+          {!isCurrentDate && <WeatherIcon src={iconSrc} alt={icon} />}
+          <p className={style.temp}>{temp}</p>
         </div>
       </div>
     );
