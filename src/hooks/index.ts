e29658b3f155ff3b5d0a82773225aaeacda0f856
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:30:32 +0300

    Reexport of hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export { useAppDispatch } from './useAppDispatch/useAppDispatch';
+export { useAppSelector } from './useAppSelector/useAppSelector';
+export { useCurrentDate } from './useCurrentDate/useCurrentDate';
