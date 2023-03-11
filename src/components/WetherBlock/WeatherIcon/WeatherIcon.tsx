a90266f269d36e403c40d4278b8430d9ac3c6325
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:09:43 +0300

    WeatherIcon component: adding prop 'alt'

--- src/components/WetherBlock/WeatherIcon/WeatherIcon.tsx
+++ src/components/WetherBlock/WeatherIcon/WeatherIcon.tsx
@@ -5,14 +5,15 @@ import style from './WeatherIcon.module.scss';
 type WeatherIconProps = {
   isCurrent?: boolean;
   src: string;
+  alt: string;
 };
 
-export const WeatherIcon = memo(({ isCurrent, src }: WeatherIconProps) => {
+export const WeatherIcon = memo(({ isCurrent, src, alt }: WeatherIconProps) => {
   const iconStyle = isCurrent ? `${style.icon} ${style.current}` : style.icon;
 
   return (
     <div className={iconStyle}>
-      <img src={src} alt="clearDayIcon" />
+      <img src={src} alt={alt} />
     </div>
   );
 });
