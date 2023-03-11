9a4007f1d530ed6be74f4cccad45340efe482749
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-11 13:30:55 +0300

    Reexport of utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
 export { getCurrentPosition } from './getCurrentPosition/getCurrentPosition';
+export type { GetPositionReturned } from './getCurrentPosition/getCurrentPosition';
 export { convertWeatherForState } from './convertWeatherForState/convertWeatherForState';
 export { getDateNowInSeconds } from './getDateNowInSeconds/getDateNowInSeconds';
 export { roundTempValue } from './roundTempValue/roundTempValue';
+export { convertUnixToTime } from './convertUnixToTime/convertUnixToTime';
+export { convertUnixToWeekDay } from './convertUnixToWeekDay/convertUnixToWeekDay';
+export { getWeatherIconLink } from './getWeatherIconLink/getWeatherIconLink';
