95864e6e418ed15146c0409a2e0b94af75ffc820
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-12 19:13:31 +0300

    Change convertUnixToTime function: - display params

--- src/utils/convertUnixToTime/convertUnixToTime.ts
+++ src/utils/convertUnixToTime/convertUnixToTime.ts
@@ -1,3 +1,7 @@
 export const convertUnixToTime = (unixTime: number): string => {
-  return new Intl.DateTimeFormat('ru-RU', { timeStyle: 'short' }).format(unixTime);
+  return new Intl.DateTimeFormat('en-SU', {
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(unixTime);
 };
