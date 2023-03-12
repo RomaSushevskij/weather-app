ede3f9cb5aff82523444e0a7057165c8d8dcb1d4
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-12 19:14:51 +0300

    Change useCurrentDate function: - time display params

--- src/hooks/useCurrentDate/useCurrentDate.ts
+++ src/hooks/useCurrentDate/useCurrentDate.ts
@@ -9,8 +9,12 @@ type CurrentDate = {
 export const useCurrentDate = (): CurrentDate => {
   const [date, setDate] = useState(new Date());
 
-  const time = date.toLocaleTimeString('ru', { timeStyle: 'short' });
-  const day = date.toLocaleDateString('en', { dateStyle: 'full' });
+  const time = date.toLocaleTimeString('en-SU', {
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const day = date.toLocaleDateString('en-SU', { dateStyle: 'full' });
 
   const refreshClock = (): void => {
     setDate(new Date());
