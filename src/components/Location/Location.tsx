9abd88ebc57bb22ad6bed03a7101fe229b7e1660
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-16 13:35:57 +0300

    Location component changing

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -2,16 +2,21 @@ import { memo } from 'react';
 
 import style from './Location.module.scss';
 
+import { Nullable } from 'types';
+
 type LocationProps = {
-  cityName: string;
-  country?: string;
+  city?: Nullable<string>;
+  country?: Nullable<string>;
 };
 
-export const Location = memo(({ cityName, country }: LocationProps) => {
+export const Location = memo(({ city, country }: LocationProps) => {
+  const isBothCityCountry = country && city;
+  const isCityOrCountry = city || country;
+
   return (
     <div className={style.location}>
-      <p className={style.city}>{cityName}</p>
-      {!!country && <p className={style.country}>{country}</p>}
+      <p className={style.city}>{isCityOrCountry}</p>
+      {isBothCityCountry && <p className={style.country}>{country}</p>}
     </div>
   );
 });
