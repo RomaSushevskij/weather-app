fec05afea3bd348e7bf0024bd568c02f471229dc
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-16 13:22:36 +0300

    Changing API - different instance

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,9 +1,21 @@
 import axios from 'axios';
 
+export const geoApify = axios.create({
+  baseURL: process.env.REACT_APP_GEO_APIFY_LINK,
+  params: {
+    apiKey: process.env.REACT_APP_GEO_APIFY_API_KEY,
+    lang: 'en',
+    limit: 1,
+    format: 'json',
+  },
+});
+
 export const openWeather = axios.create({
   baseURL: process.env.REACT_APP_OPEN_WEATHER_LINK,
   params: {
     appid: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
+    units: 'metric',
+    exclude: 'minutely',
   },
 });
 
