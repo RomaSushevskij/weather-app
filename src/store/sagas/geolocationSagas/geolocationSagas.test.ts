6ddbbdc340981e219a4b7e7561397ee053f02fea
RomaSushevskij <roma.sushevskij@yandex.ru>
2023-03-24 14:33:03 +0300

    Test for geolocationSagas

--- src/store/sagas/geolocationSagas/geolocationSagas.test.ts
+++ src/store/sagas/geolocationSagas/geolocationSagas.test.ts
@@ -6,10 +6,13 @@ import { fetchGeolocation, FetchGeolocationParams } from './geolocationSagas';
 
 import { geocodeAPI } from 'api';
 import { Geolocation } from 'api/geocodeAPI/types';
+import { errorMessages } from 'enum';
+import { appAC } from 'store/reducers/appReducer/appReducer';
 import { getCurrentPosition, GetPositionReturned } from 'utils';
 
-test('Call fetchGeolocation with empty localityName', () => {
-  const locality: FetchGeolocationParams = { localityName: '' };
+describe('fetchGeolocation should work correct', () => {
+  const localityEmpty: FetchGeolocationParams = { localityName: '' };
+  const localityFull: FetchGeolocationParams = { localityName: 'Minsk' };
   const position: GetPositionReturned = { latitude: 10, longitude: 20 };
   const geoLocation: Geolocation = {
     lat: position.latitude,
@@ -18,19 +21,47 @@ test('Call fetchGeolocation with empty localityName', () => {
     country: 'Country',
   };
 
-  const gen = fetchGeolocation(locality);
+  it('Call fetchGeolocation with full localityName', () => {
+    const gen = fetchGeolocation(localityFull);
 
-  expect(gen.next().value).toEqual(call(getCurrentPosition));
-  // @ts-ignore
-  expect(gen.next(position).value).toEqual(
-    call(geocodeAPI.reverse, {
-      latitude: position.latitude,
-      longitude: position.longitude,
-    }),
-  );
-  // @ts-ignore
-  expect(gen.next(geoLocation).value).toEqual(
-    put(geolocationAC.setLocation(geoLocation)),
-  );
-  expect(gen.next().done).toBeTruthy();
+    expect(gen.next().value).toEqual(
+      call(geocodeAPI.search, {
+        localityName: localityFull.localityName,
+      }),
+    );
+    // @ts-ignore
+    expect(gen.next(geoLocation).value).toEqual(
+      put(geolocationAC.setLocation(geoLocation)),
+    );
+  });
+
+  it('Call fetchGeolocation with empty localityName', () => {
+    const gen = fetchGeolocation(localityEmpty);
+
+    expect(gen.next().value).toEqual(call(getCurrentPosition));
+    // @ts-ignore
+    expect(gen.next(position).value).toEqual(
+      call(geocodeAPI.reverse, {
+        latitude: position.latitude,
+        longitude: position.longitude,
+      }),
+    );
+    // @ts-ignore
+    expect(gen.next(geoLocation).value).toEqual(
+      put(geolocationAC.setLocation(geoLocation)),
+    );
+    expect(gen.next().done).toBeTruthy();
+  });
+
+  it('Call fetchGeolocation with error', () => {
+    const gen = fetchGeolocation(localityFull);
+
+    const errorMessage = new TypeError('Request failed');
+
+    gen.next();
+    expect(gen.throw(errorMessage).value).toEqual(
+      put(appAC.setErrorMessage({ errorMessage: errorMessages.NOTHING_FOUND })),
+    );
+    expect(gen.next().value).toEqual(put(appAC.setStatus({ status: 'failed' })));
+  });
 });
